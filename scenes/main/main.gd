0b0f2fdd640aa11752ed99b0b1d76fa8827abe60
Brandon Chang <brandon@brandonchang.me>
2024-02-18 10:35:37 -0800

    fonts

--- scenes/main/main.gd
+++ scenes/main/main.gd
@@ -1,16 +1,3 @@
 extends Node
 
-@export var max_discards := 5
-
 var total := 0.0
-var discards := 0 :
-	set(value):
-		discards = value
-		Events.discards_changed.emit(discards)
-		%Discards.text = "Discards: %s" % [discards]
-
-func _ready():
-	Events.item_scanned.connect(_on_item_scanned)
-
-func _on_item_scanned(_item) -> void:
-	discards = max_discards
