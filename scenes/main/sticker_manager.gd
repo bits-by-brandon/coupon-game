df9eb8deee9dde969b763c21cebf714978d687b3
Brandon Chang <brandon@brandonchang.me>
2024-02-19 23:02:25 -0800

    confetti

--- scenes/main/sticker_manager.gd
+++ scenes/main/sticker_manager.gd
@@ -7,9 +7,15 @@ func _ready():
 
 func _on_item_purchased(item : ItemEntity, _coupons):
 	await get_tree().create_timer(.2).timeout
-	if item.is_invalid() || item.current_discount == 0:
+
+	var percent_off := item.current_discount / item.base_price
+	if percent_off > 1:
 		animation_player.play("full_price")
-	elif item.is_full_discount():
+	elif percent_off == 1:
 		animation_player.play("full_discount")
-	else:
+	elif percent_off > .7:
+		animation_player.play("great_deal")
+	elif percent_off > .5:
 		animation_player.play("good_deal")
+	elif percent_off == 0:
+		animation_player.play("full_price")
